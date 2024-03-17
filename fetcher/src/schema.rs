5c4b34b6b229b2e1f72dd135762012b1fa3d4cb4
canpok1 <6isstrong@gmail.com>
2024-03-17 19:00:50 +0900

    migration用sqlをmigrator配下に移動

--- fetcher/src/schema.rs
+++ fetcher/src/schema.rs
@@ -26,4 +26,7 @@ diesel::table! {
 
 diesel::joinable!(daily_rates -> rate_types (rate_type_id));
 
-diesel::allow_tables_to_appear_in_same_query!(daily_rates, rate_types,);
+diesel::allow_tables_to_appear_in_same_query!(
+    daily_rates,
+    rate_types,
+);
